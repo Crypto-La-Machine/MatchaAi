1db6ac5abd1ebf46c91e92b786005b776bcf453a
Matthieu Pilleul <matthieu.pilleul@epitech.eu>
2024-10-13 23:16:24 +0200

    Update button color text

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
         <div className="buttons">
           <button className="address-button" onClick={() => handleCopy(false)}>
             <span className="icon-copy">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
                 <rect width="13" height="13" x="8" y="8" rx="2" ry="2" />
                 <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
               </svg>
