1a6d7758be04ce1dd9b30f47c702608baf9eb537
Matthieu Pilleul <matthieu.pilleul@epitech.eu>
2024-10-14 17:47:42 +0200

    Update socials and address format

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import gsap from "gsap";
 function App() {
   const container = useRef();
   const [loading, setLoading] = useState(true);
-  const [copyText, setCopyText] = useState("0x..E67");
-
-  const fullTokenAddress = '0X57689087675465768798UH755565768798';
+  const fullTokenAddress = '0x57689087675465768798UH755565768798';
+  const [copyText, setCopyText] = useState(`${fullTokenAddress.slice(0, 3)}...${fullTokenAddress.slice(-3)}`);
   const [formattedAddress, setFormattedAddress] = useState(fullTokenAddress);
 
   const formatTokenAddress = () => {
@@ -85,7 +84,7 @@ function App() {
     navigator.clipboard.writeText(fullTokenAddress).then(() => {
       if (!isFooter) {
         setCopyText("COPIED");
-        setTimeout(() => setCopyText("0x..E67"), 1500);
+        setTimeout(() => setCopyText(`${fullTokenAddress.slice(0, 3)}...${fullTokenAddress.slice(-3)}`), 1500);
       }
     });
   };
@@ -140,21 +139,20 @@ function App() {
 
       <div className="footer">
         <div className="social-icons">
-          <a href="https://example.com" target="_blank" rel="noopener noreferrer" className="icon">
-            <svg enableBackground="new 0 0 1668.56 1221.19" viewBox="0 0 1668.56 1221.19" width="30" height="30" fill="#fff">
-              <path d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99
-		h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z"></path>
+          <a href="https://x.com/matchaai_io" target="_blank" rel="noopener noreferrer" className="icon">
+            <svg enableBackground="new 0 0 1668.56 1221.19" viewBox="0 0 1668.56 1221.19" fill="#fff">
+              <path d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99 h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z"></path>
             </svg>
           </a>
-          <a href="https://example.com" target="_blank" rel="noopener noreferrer" className="icon">
-            <svg viewBox="0 0 24 24" width="27.5" height="27.5" fill="none">
+          <a href="https://t.me/matchaai_io" target="_blank" rel="noopener noreferrer" className="icon">
+            <svg viewBox="0 0 24 24" fill="none">
               <path d="M16.114 9.291c.552-.552 1.1-1.84-1.2-.276a395.806 395.806 0 0 1-6.489 4.372 2.7 2.7 0 0 1-2.117.046c-1.38-.414-2.991-.966-2.991-.966s-1.1-.691.783-1.427c0 0 7.961-3.267 10.722-4.418 1.058-.46 4.647-1.932 4.647-1.932s1.657-.645 1.519.92c-.046.644-.414 2.9-.782 5.338-.553 3.451-1.151 7.225-1.151 7.225s-.092 1.058-.874 1.242a3.787 3.787 0 0 1-2.3-.828c-.184-.138-3.451-2.209-4.648-3.221a.872.872 0 0 1 .046-1.473 169.31 169.31 0 0 0 4.835-4.602Z"></path>
             </svg>
           </a>
         </div>
         <div className="footer-info">
           <span className="footer-item footer-left">Matcha AI © All Rights Reserved</span>
-          <span className="footer-item footer-center">contact@matchaai.io</span>
+          <a href="mailto:contact@matchaai.io" className="footer-item footer-center">contact@matchaai.io</a>
           <span
             className="footer-item footer-right"
             id="tokenAddress"
